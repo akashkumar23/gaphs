52e58f06eae30bf2f1494d8ee7c01b72476513d0
Akash Kumar <77935092+akashkumar23@users.noreply.github.com>
2022-06-14 22:29:13 +0530

    date:14-06-22 shortest path

---
+++ cses_1667.cpp
@@ -0,0 +1,358 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define lld long long double
+#define fi first
+#define se second
+#define yes cout<<"YES"<<'\n'
+#define no cout<<"NO"<<'\n'
+#define fl(i,a,b) for(ll i=a; i<b; i++)
+#define fle(a,b) for(ll i=a; i<=b; i++)
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vld vector<ld>
+#define vs vector<string>
+#define vpll vector<pair<ll,ll>>
+#define mll map<ll,ll>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define ceil(a,b) (a+b-1)/b
+#define intake(a,n) ll n; cin>>n; vector<ll> a(n); for(ll i=0;i<n;i++) cin>>a[i]
+#define intake2(a,n,k) ll n,k; cin>>n>>k; vector<ll> a(n); for(ll i=0;i<n;i++) cin>>a[i]
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pq priority_queue
+#define loop(n) for(int i=0;i<n;i++)
+#define out(a) for(int i=0;i<a.size();i++) cout<<a[i]<<" "; cout<<"\n"
+#define en "\n"
+// #define sp <<" "
+#define sorti(a) sort(a.begin(),a.end())
+#define sortd(a) sort(a.rbegin(),a.rend())
+#define get_sum(a) accumulate(a.begin(),a.end(),0LL)
+#define get_max(a) *max_element(a.begin(),a.end())
+#define get_min(a) *min_element(a.begin(),a.end())
+#define get_max_pos(v) max_element(v.begin(), v.end()) - v.begin()
+#define unique(s) unique(s.begin(), s.end()) - s.begin() // retunrns the # of unique blocks elements
+#define setbits(x) __builtin_popcountll(x)
+#define ceil(a,b) (a+b-1)/b
+const int mod = 1000000007;
+const int dom = 998244353;
+const ll inf = 2100000000;
+const ld eps = 1e-6, pi = 3.14159265358979323846264338327950;
+const long long INF = 4000000000000000000LL;
+
+bool even(ll n){
+	return (!(n&1));
+}
+
+void precision(int a) {cout << setprecision(a) << fixed;}
+
+
+ll pw(ll a, ll b, ll m){
+	//O(log b)
+	if(b==0){
+		return a%m;
+	}
+	if(b%2==0){
+		ll t = pw(a,b/2, m);
+		return ((t*t)%m);
+	}
+	else{
+		ll t = pw(a,(b-1)/2,m);
+		return (a*t%m);
+	}
+}
+
+
+ll binaryexp(ll a, ll b)
+{
+    ll ans = 0;
+    if (b == 0)
+        return 1;
+    else if (b == 1)
+        return a;
+    else if (b % 2)
+    {
+        ans = (a * (binaryexp((a * a) % mod, b / 2) % mod)) % mod;
+    }
+    else
+        ans = binaryexp((a * a) % mod, b / 2) % mod;
+    return ans % mod;
+}
+
+// inverse of a number only if gcd(b,m) = 1 // O(log m)
+ll inv(ll b, ll m){
+	return pw(b,m-2,m);
+}
+
+
+// PRIME CHECK
+bool prime(ll n){
+	bool isPrime = true;
+	fle(2,sqrt(n)){
+		if(n%i==0){
+			isPrime = false;
+			break;
+		}
+	}
+	return isPrime;
+}
+
+// Converting decimal to binary string
+string intToBinary(int n)
+{
+    string s = "";
+    while (n > 0)
+    {
+        s =  ( (n % 2 ) == 0 ? "0" : "1") +s;
+        n = n / 2;
+    }
+    return s;
+}
+
+// FINDING DIVISORS OF N
+void divisors_of_n(ll n){
+	vector<ll>divisors;
+	fle(1,sqrt(n)){
+		if(n%i == 0){
+			divisors.push_back(i);
+			if(i*i != n){
+				divisors.push_back(n/i);
+			}
+		}
+	}
+	for(auto divisor : divisors){
+		cout<<divisor<<" ";
+	}
+}
+
+//PRIME FACTORIZATION
+void prime_factors(ll n){
+	vector<pair<int,int>> factorization;
+	for(int i=2; i*i <= n; i++){
+    	int count = 0;
+    	while(n % i == 0){
+        	count++;
+        	n /= i;
+    	}
+    	if(count > 0){
+        	factorization.push_back({i, count});
+    	}
+	}
+	if(n != 1){
+    	factorization.push_back({n, 1});
+	}
+	for(auto factor : factorization){
+		cout<<factor.first<<" "<<factor.second<<'\n';
+	}
+
+}
+
+// Sieve of Eratosthenes
+void SOE(ll n){
+	const ll lim = 1e7 + 10;
+	vector<bool>isprime(lim, true);
+	isprime[0] = isprime[1] = false;
+	for(ll i=2; i*i<=n; i++){
+		if(isprime[i]){
+			for(ll j=i*i; j<=n; j+=i){
+				isprime[j] = false;
+			}
+		}
+		
+	for(ll i=0; i<=n; i++){
+		if(isprime[i]){
+			cout<<i<<" ";
+		}
+	}
+	} 	
+}
+
+//Smallest Prime Factor
+void SPF(ll n){
+	const ll lim = 1e7 + 10;
+	vector<ll>spf(lim);
+	for(ll i=0; i<lim; i++){
+		spf[i]=i;
+	}
+	for(ll i=2; i*i<=n; i++){
+		if(spf[i]==i){
+			for(ll j=i*i; j<=n; j+=i){
+				if(spf[j]==j){
+					spf[j]=i;
+				}
+			}
+		}
+	} 
+	cout<<spf[n];
+}
+
+ll nCr(ll n, ll r)
+{
+    if (n < r)
+        return 0;
+    if (r > n - r)
+        r = n - r;
+    ll ans = 1;
+    ll i;
+    for (i = 1; i <= r; i++)
+    {
+        ans *= n - r + i;
+        ans /= i;
+    }
+    return ans;
+}
+
+ll nfac(ll n, ll mod){
+	ll ans = 1;
+	for(ll i=1; i<=n; i++){
+		ans = (ans%mod * i%mod)%mod;
+	}
+	return ans;
+}
+
+ll IPOT(ll x){
+	if(x&(x-1)){
+		return 0;
+	}
+	return 1;
+}
+
+// x|(1<<k) : sets the kth bit
+// x&~(1<<k): unsets the kth bit
+// x^(1<<k) : inverts the kth bit
+
+// IN BIT-MANIPULATION : THINK BITS ARE INDEPENDENT OF EACH OTHER. 
+// FIRST THINK ALGO THEN IMPLEMENT 
+
+//*****************solved below********************//
+// DONT CODE RANDOMLY SOMETHING FOOLISH, YOU BITCH
+
+// vector<vector<int>>arr(n,vector<int>(m));
+//    fi(i,n)fi(j,m)cin>>arr[i][j];
+
+
+ll n,m;
+vector<vector<ll>>adj;
+vector<ll>vis;
+vector<ll>dist;
+vector<ll>parent;
+vector<ll>path;
+
+void bfs(ll node){
+	
+	dist[node] = 0;
+	queue<ll>q;
+	
+	q.push(node);
+	vis[node] = 1;
+	
+	while(!q.empty()){
+		ll x = q.front();
+		q.pop();
+		for(ll i=0; i<adj[x].size(); i++){
+			
+			ll child = adj[x][i];
+			
+			if( vis[child]==0 ){
+				
+				q.push(child);
+				vis[child] = 1;
+				parent[child]=x;
+				
+				dist[child] = dist[x] + 1;
+			}
+		}
+	}
+	
+	
+}
+
+void trace(ll final_node){
+	
+	if(parent[final_node] == -1){
+		return;
+	}
+	
+	path.pb(parent[final_node]);
+	trace(parent[final_node]);
+}
+
+void solve(){
+	// source S and destination F is given in the problem .
+	ll s = 0;
+	// cin>>s>>f;
+	cin>>n>>m;
+	
+	adj.resize(n);
+	vis.resize(n);
+	dist.resize(n);
+	parent.resize(n);
+	
+	//initializing parent of all node with -1
+	for(ll i=0; i<n; i++){
+		parent[i] = -1;
+	}
+	
+	for(ll i=0; i<m; i++){
+		ll u,v;
+		cin>>u>>v;
+		u--;
+		v--;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	
+	bfs(s);
+	
+	
+	if(dist[n-1]+1 == 1){
+		cout<<"IMPOSSIBLE";
+		return;
+	}
+	
+	cout<<dist[n-1] + 1<<en;
+	
+	path.push_back(n-1);
+	trace(n-1);
+	reverse(path.begin() , path.end());
+	
+	
+	for(auto &x : path){
+		cout<<x + 1<<" ";
+	}
+	
+	
+	
+	
+	
+}
+	
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	ll q=1;
+    // cin>>q;
+	
+	while(q--){
+		solve();
+		cout<<en;
+	}
+	return 0;
+}
+
+// i:
+// 1
+// 0 3
+// 5 5
+// 1 2
+// 0 2
+// 2 3
+// 1 3
+// 1 4
+// 
+// o:
+// 2
+// 0 2 3 
---
+++ shortest_path_DAG_weighted.cpp
@@ -0,0 +1,390 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define lld long long double
+#define fi first
+#define se second
+#define yes cout<<"YES"<<'\n'
+#define no cout<<"NO"<<'\n'
+#define fl(i,a,b) for(ll i=a; i<b; i++)
+#define fle(a,b) for(ll i=a; i<=b; i++)
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vld vector<ld>
+#define vs vector<string>
+#define vpll vector<pair<ll,ll>>
+#define mll map<ll,ll>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define ceil(a,b) (a+b-1)/b
+#define intake(a,n) ll n; cin>>n; vector<ll> a(n); for(ll i=0;i<n;i++) cin>>a[i]
+#define intake2(a,n,k) ll n,k; cin>>n>>k; vector<ll> a(n); for(ll i=0;i<n;i++) cin>>a[i]
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pq priority_queue
+#define loop(n) for(int i=0;i<n;i++)
+#define out(a) for(int i=0;i<a.size();i++) cout<<a[i]<<" "; cout<<"\n"
+#define en "\n"
+// #define sp <<" "
+#define sorti(a) sort(a.begin(),a.end())
+#define sortd(a) sort(a.rbegin(),a.rend())
+#define get_sum(a) accumulate(a.begin(),a.end(),0LL)
+#define get_max(a) *max_element(a.begin(),a.end())
+#define get_min(a) *min_element(a.begin(),a.end())
+#define get_max_pos(v) max_element(v.begin(), v.end()) - v.begin()
+#define unique(s) unique(s.begin(), s.end()) - s.begin() // retunrns the # of unique blocks elements
+#define setbits(x) __builtin_popcountll(x)
+#define ceil(a,b) (a+b-1)/b
+const int mod = 1000000007;
+const int dom = 998244353;
+const ll inf = 2100000000;
+const ld eps = 1e-6, pi = 3.14159265358979323846264338327950;
+const long long INF = 4000000000000000000LL;
+
+bool even(ll n){
+	return (!(n&1));
+}
+
+void precision(int a) {cout << setprecision(a) << fixed;}
+
+
+ll pw(ll a, ll b, ll m){
+	//O(log b)
+	if(b==0){
+		return a%m;
+	}
+	if(b%2==0){
+		ll t = pw(a,b/2, m);
+		return ((t*t)%m);
+	}
+	else{
+		ll t = pw(a,(b-1)/2,m);
+		return (a*t%m);
+	}
+}
+
+
+ll binaryexp(ll a, ll b)
+{
+    ll ans = 0;
+    if (b == 0)
+        return 1;
+    else if (b == 1)
+        return a;
+    else if (b % 2)
+    {
+        ans = (a * (binaryexp((a * a) % mod, b / 2) % mod)) % mod;
+    }
+    else
+        ans = binaryexp((a * a) % mod, b / 2) % mod;
+    return ans % mod;
+}
+
+// inverse of a number only if gcd(b,m) = 1 // O(log m)
+ll inv(ll b, ll m){
+	return pw(b,m-2,m);
+}
+
+
+// PRIME CHECK
+bool prime(ll n){
+	bool isPrime = true;
+	fle(2,sqrt(n)){
+		if(n%i==0){
+			isPrime = false;
+			break;
+		}
+	}
+	return isPrime;
+}
+
+// Converting decimal to binary string
+string intToBinary(int n)
+{
+    string s = "";
+    while (n > 0)
+    {
+        s =  ( (n % 2 ) == 0 ? "0" : "1") +s;
+        n = n / 2;
+    }
+    return s;
+}
+
+// FINDING DIVISORS OF N
+void divisors_of_n(ll n){
+	vector<ll>divisors;
+	fle(1,sqrt(n)){
+		if(n%i == 0){
+			divisors.push_back(i);
+			if(i*i != n){
+				divisors.push_back(n/i);
+			}
+		}
+	}
+	for(auto divisor : divisors){
+		cout<<divisor<<" ";
+	}
+}
+
+//PRIME FACTORIZATION
+void prime_factors(ll n){
+	vector<pair<int,int>> factorization;
+	for(int i=2; i*i <= n; i++){
+    	int count = 0;
+    	while(n % i == 0){
+        	count++;
+        	n /= i;
+    	}
+    	if(count > 0){
+        	factorization.push_back({i, count});
+    	}
+	}
+	if(n != 1){
+    	factorization.push_back({n, 1});
+	}
+	for(auto factor : factorization){
+		cout<<factor.first<<" "<<factor.second<<'\n';
+	}
+
+}
+
+// Sieve of Eratosthenes
+void SOE(ll n){
+	const ll lim = 1e7 + 10;
+	vector<bool>isprime(lim, true);
+	isprime[0] = isprime[1] = false;
+	for(ll i=2; i*i<=n; i++){
+		if(isprime[i]){
+			for(ll j=i*i; j<=n; j+=i){
+				isprime[j] = false;
+			}
+		}
+		
+	for(ll i=0; i<=n; i++){
+		if(isprime[i]){
+			cout<<i<<" ";
+		}
+	}
+	} 	
+}
+
+//Smallest Prime Factor
+void SPF(ll n){
+	const ll lim = 1e7 + 10;
+	vector<ll>spf(lim);
+	for(ll i=0; i<lim; i++){
+		spf[i]=i;
+	}
+	for(ll i=2; i*i<=n; i++){
+		if(spf[i]==i){
+			for(ll j=i*i; j<=n; j+=i){
+				if(spf[j]==j){
+					spf[j]=i;
+				}
+			}
+		}
+	} 
+	cout<<spf[n];
+}
+
+ll nCr(ll n, ll r)
+{
+    if (n < r)
+        return 0;
+    if (r > n - r)
+        r = n - r;
+    ll ans = 1;
+    ll i;
+    for (i = 1; i <= r; i++)
+    {
+        ans *= n - r + i;
+        ans /= i;
+    }
+    return ans;
+}
+
+ll nfac(ll n, ll mod){
+	ll ans = 1;
+	for(ll i=1; i<=n; i++){
+		ans = (ans%mod * i%mod)%mod;
+	}
+	return ans;
+}
+
+ll IPOT(ll x){
+	if(x&(x-1)){
+		return 0;
+	}
+	return 1;
+}
+
+// x|(1<<k) : sets the kth bit
+// x&~(1<<k): unsets the kth bit
+// x^(1<<k) : inverts the kth bit
+
+// IN BIT-MANIPULATION : THINK BITS ARE INDEPENDENT OF EACH OTHER. 
+// FIRST THINK ALGO THEN IMPLEMENT 
+
+//*****************solved below********************//
+// DONT CODE RANDOMLY SOMETHING FOOLISH, YOU BITCH
+
+// vector<vector<int>>arr(n,vector<int>(m));
+//    fi(i,n)fi(j,m)cin>>arr[i][j];
+
+ll n,m;
+vector<vll>adj;
+vector<bool>vis;
+stack<ll>st;
+vll topo;
+map<pair<ll,ll>, ll>mp;
+vector<ll>dis;
+vll parent;
+vll path;
+
+void dfs(ll node){
+	
+	vis[node] = true;
+	for(auto &child : adj[node]){
+		if(!vis[node]){
+			dfs(child);
+		}
+	}
+	
+	st.push(node);
+	
+}
+
+
+void trace(ll final_node){
+	
+	if(parent[final_node] == -1){
+		return;
+	}
+	
+	path.pb(parent[final_node]);
+	trace(parent[final_node]);
+}
+
+void solve(){
+	
+	ll s,f;
+	cin>>s>>f;
+	cin>>n>>m;
+	
+	adj.resize(n);
+	vis.resize(n);
+	dis.resize(n);
+	parent.resize(n);
+	
+	// setting parent of ech node to -1
+	for(ll i=0; i<n; i++){
+		parent[i] = -1;
+	}
+	
+	// innitializing distace with infinitity 
+	for(ll i=0; i<n; i++){
+		dis[i] = 1e9;
+	}
+	
+	dis[s] = 0;
+	
+	// inputting graph with weights
+	for(ll i=0; i<m; i++){
+		ll u,v,w;
+		cin>>u>>v>>w;
+		adj[u].pb(v);
+		mp[{u,v}] = w;
+	}
+	
+	// running dfs for topological sort
+	for(ll i=0; i<n; i++){
+		if(!vis[i]){
+			dfs(i);
+		}
+	}
+	
+	// topological sort : entering values in order , in topo
+	while(!st.empty()){
+		topo.push_back(st.top());
+		st.pop();
+	}
+	
+	reverse(all(topo));
+	
+	// // topological ordering 
+	// for(auto &x : topo){
+		// cout<<x<<" ";
+	// }
+	// cout<<en;
+// 	
+	// // adjcencency list with w/h;
+	// for(ll i=0; i<n; i++){
+		// for(auto &x : adj[i]){
+			// cout<<i<<" : "<<x<<" w/h "<<mp[{i,x}]<<en;;
+		// }
+		// cout<<en;
+	// }
+	
+	
+	// applying relaxation 
+	for(ll i=0; i<n; i++){
+		
+		ll u = topo[i];
+		for(auto &v : adj[u]){
+			ll w = mp[{u,v}];
+			
+			if(dis[u] + w < dis[v]){
+				dis[v] = dis[u] + w;
+				parent[v] = u;
+			}
+			
+		}
+	
+		
+	}
+	
+	// printing  mincost distance
+	cout<<dis[f]<<en;
+	
+	//tracing path
+	path.push_back(f);
+	trace(f);
+	reverse(path.begin(), path.end());
+	
+	//printing path
+	for(auto &x : path){
+		cout<<x<<" ";
+	}
+	
+}
+	
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	ll q;
+    cin>>q;
+	
+	while(q--){
+		solve();
+		cout<<en;
+	}
+	return 0;
+}
+
+
+// io
+// 1
+// 0 1
+// 5 5
+// 0 1 100
+// 0 2 20
+// 3 1 10
+// 2 3 45
+// 3 4 20
+// 
+// op
+// 75
+// 0 2 3 1
+
